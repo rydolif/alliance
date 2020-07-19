ffb0ca9cf442cd52d5687ee110e193b0f4387dd3
master <rudolifrudolif@gmail.com>
2020-07-19 22:58:33 +0300

    optimaze + preloader + edits

--- app/js/common.js
+++ app/js/common.js
@@ -146,3 +146,9 @@ $(function() {
 
   
 });
+
+
+//----------------------------------------preloader----------------------------------
+  $(window).on('load', function(){
+    $('.preloader').delay(1000).fadeOut('slow');
+  });
